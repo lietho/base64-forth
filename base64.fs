7233a067640ac654ad428571dd5982ac6db1f4f2
Thomas Lielacher <thomas.lielacher@gmail.com>
2020-10-22 16:25:27 +0200

    implemented map-value

--- base64.fs
+++ base64.fs
@@ -1,5 +1,11 @@
+: map-value ( u -- c)
+\ maps a value between 0 and 63 to the corresponding character
+    s" ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+    drop swap
+    chars + @ ;
+
 : base64-encode ( addr u -- addr u )
-    2dup 2drop ;
+    noop ;
 
 : base64-decode ( addr u -- addr u )
-    2dup 2drop ;
+    noop ;
