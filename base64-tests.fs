0feb42d428c84362bc42325b382b9379ea4b68d5
Thomas Lielacher <thomas.lielacher@gmail.com>
2020-10-29 14:28:41 +0100

    implemented some helper words

--- base64-tests.fs
+++ base64-tests.fs
@@ -6,8 +6,28 @@
     endif ;
 
 
+s" Zm9vYg=="  base64-padding-char-num 2 <> throw
+s" Zm9vYmE="  base64-padding-char-num 1 <> throw
+s" Zm9vYmFy"  base64-padding-char-num 0 <> throw
+
+s" "        base64-encode-len 0 <> throw
+s" f"       base64-encode-len 4 <> throw
+s" fo"      base64-encode-len 4 <> throw
+s" foo"     base64-encode-len 4 <> throw
+s" foob"    base64-encode-len 8 <> throw
+s" fooba"   base64-encode-len 8 <> throw
+s" foobar"  base64-encode-len 8 <> throw
+
+s" "          base64-decode-len 0 <> throw
+s" Zg=="      base64-decode-len 1 <> throw
+s" Zm8="      base64-decode-len 2 <> throw
+s" Zm9v"      base64-decode-len 3 <> throw
+s" Zm9vYg=="  base64-decode-len 4 <> throw
+s" Zm9vYmE="  base64-decode-len 5 <> throw
+s" Zm9vYmFy"  base64-decode-len 6 <> throw
+
 \ taken from https://tools.ietf.org/html/rfc4648#section-10
-s" " base64-encode s" " test
+s" "        base64-encode s" "          test
 s" f"       base64-encode s" Zg=="      test
 s" fo"      base64-encode s" Zm8="      test
 s" foo"     base64-encode s" Zm9v"      test
@@ -15,6 +35,7 @@ s" foob"    base64-encode s" Zm9vYg=="  test
 s" fooba"   base64-encode s" Zm9vYmE="  test
 s" foobar"  base64-encode s" Zm9vYmFy"  test
 
+s" "          base64-decode s" "        test
 s" Zg=="      base64-decode s" f"       test
 s" Zm8="      base64-decode s" fo"      test
 s" Zm9v"      base64-decode s" foo"     test
--- base64.fs
+++ base64.fs
@@ -1,11 +1,46 @@
-: map-value ( u -- c)
+: base64-padding-char ( -- c) '=' ;
+
+: normalize-char ( c -- c) 256 mod ;
+
+: base64-padding-char-num ( addr u -- u)
+\ gets the number of the padding characters at the end of the specified string
+    0 >r
+    begin
+        1- 2dup
+        chars + @ normalize-char
+        base64-padding-char <> dup invert
+        if
+            r> 1+ >r
+        endif
+    until 
+    2drop
+    r> ;
+
+: base64-map-value ( u -- c)
 \ maps a value between 0 and 63 to the corresponding character
     s" ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
     drop swap
     chars + @ ;
 
+: base64-encode-len ( addr u -- u )
+\ calculates the size of the base64 encoded string by the size of the input
+    nip 3 /mod swap 0>
+    if 1+ endif
+    4 * ;
+
+: base64-decode-len ( addr u -- u )
+\ calculates the size of the decoded string by the size of the encoded string ((3 * (LengthInCharacters / 4)) - (numberOfPaddingCharacters))
+   2dup base64-padding-char-num >r
+   4 / 3 * r> - ;
+
 : base64-encode ( addr u -- addr u )
-    noop ;
+\ IDEA: Allocate memory for result, iterate over input, map values, set corresponding value in result
+    here swap base64-encode-len dup chars allot \ reserve memory for the encoded string
+    rot drop \ just cleaning up as long we aren't finished
+    ;
 
 : base64-decode ( addr u -- addr u )
-    noop ;
+\ IDEA: Allocate memory for result, iterate over input, map values, set corresponding value in result
+    here swap base64-decode-len dup chars allot \ reserve memory for the encoded string
+    rot drop \ just cleaning up as long we aren't finished
+    ;
